091dea4fa7851fd60dee048fc7e4c12566432752
Michiel Van den Berghe <michiel@trackuity.com>
2018-10-04 21:40:37 +0200

    Massive cleanup, introduce UFO image

--- visualization/visualization.js
+++ visualization/visualization.js
@@ -1,5 +1,5 @@
 // Initialize Firebase
-var config = {
+const firebaseConfig = {
   apiKey: "AIzaSyCry7RclR4ZOh4A956n7mqugWUExLAba_0",
   authDomain: "kraakkrook-c581e.firebaseapp.com",
   databaseURL: "https://kraakkrook-c581e.firebaseio.com",
@@ -7,217 +7,198 @@ var config = {
   storageBucket: "kraakkrook-c581e.appspot.com",
   messagingSenderId: "610352027337"
 };
-firebase.initializeApp(config);
-var db = firebase.database();
+const ufoSize = 100;
+const levels = [-2, -1, 0, 1, 2, 3];
 
-// maps levels to a list of beacons
-const sites = {};
-// maps krookids to levels
-const siteToLevel = {};
-const movedFuncs = {};
-// maps sensor id to actual sensor locations
-const sensors = {};
-
-var getSensorLocationsForLevel = function getSensorLocationsForLevel(level) {
-  return Object.keys(sensors)
-    .map(function sensorIdToObject(sensorId) {
-      return sensors[sensorId];
-    })
-    .filter(function filterSensorByLevel(sensor) {
-      return sensor.level == level;
-    });
-}
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+// level -> list of beacons
+let beaconsByLevel = {};
+const beaconById = {};
 
 
-function init() {
-  var voronoi = d3.voronoi();
+const linkLengthSquared = (source, target) => {
+  const dx = target[0] - source[0];
+  const dy = target[1] - source[1];
+  return dx * dx + dy * dy;
+};
 
-  setTimeout(function() { window.location.reload(true); }, 120000);
+const svgForLevel = level => d3.select((`#svg_${level}`).replace('-', '_'));
 
-  function redrawSite(site) {
-    site
-      .attr("cx", function (d) { return d[0]; })
-      .attr("cy", function (d) { return d[1]; });
-  }
+const drawLevels = beaconsByLevel => {
+  const voronoi = d3.voronoi();
 
-  Object.keys(sites).forEach(function eachSite(level) {
-    function moved() {
-      redraw();
-    }
-    movedFuncs[level] = moved;
+  levels.forEach(level => {
+    const svg = svgForLevel(level);
+    const beacons = beaconsByLevel[level] || [];
+    // draw links
+    const links = voronoi
+      .links(beacons.map(b => [b.x, b.y]))
+      .filter(l => linkLengthSquared(l.source, l.target) < 200000)
 
-    function redraw() {
-      var diagram = voronoi(sites[level]);
-      var sensorLocations = getSensorLocationsForLevel(level)
-      var triangleData = sensorLocations.map(l => l.closestSites);
-      triangle = triangle.data(triangleData), triangle.exit().remove();
-      triangle = triangle.enter().append("path").merge(triangle).call(redrawtriangle);
-      link = link.data(diagram.links()), link.exit().remove();
-      link = link.enter().append("line").merge(link).call(redrawlink);
-      site = site.data(sites[level]).call(redrawSite);
-      sensor = sensor.data(sensorLocations, sl => {
-        return sl.sensorId
-      }).call(redrawSensor);
-      sensor.exit().remove();
-      sensor = sensor.enter()
-        .append("circle")
-        .attr("r", 10)
-        .call(redrawSensor);
-    }
-
-    function redrawtriangle(triangle) {
-      triangle
-        .attr("class", "primary")
-        .attr("d", function (d) { return "M" + d.join("L") + "Z"; });
-    }
-
-    function redrawlink(link) {
-      link
-        // .classed("primary", function (d) { return d.source === sites[level][0] || d.target === sites[level][0]; })
-        .attr("x1", function (d) { return d.source[0]; })
-        .attr("y1", function (d) { return d.source[1]; })
-        .attr("x2", function (d) { return d.target[0]; })
-        .attr("y2", function (d) { return d.target[1]; });
-    }
-
-    function redrawSensor(sensor) {
-      sensor
-        .attr("cx", function (d) { return d.coords[0] })
-        .attr("cy", function (d) { return d.coords[1]; });
-    }
-
-    var svg =
-      d3.select(("#svg_" + level).replace('-', '_')),
-      width = +svg.attr("width"),
-      height = +svg.attr("height");
-
-    var triangle = svg.append("g")
-      .attr("class", "triangles")
-      .selectAll("path")
-      // .data(voronoi.triangles(sites[level]))
-      // .enter().append("path")
-      // .call(redrawtriangle);
-
-    var link = svg.append("g")
+    // draw links between beacons
+    svg.append("g")
       .attr("class", "links")
       .selectAll("line")
-      .data(voronoi.links(sites[level]))
-      .enter().append("line")
-      .call(redrawlink);
-
-    var site = svg.append("g")
+      .data(links)
+      .enter()
+        .append("line")
+        .attr("x1", d => d.source[0])
+        .attr("y1", d => d.source[1])
+        .attr("x2", d => d.target[0])
+        .attr("y2", d => d.target[1]);
+    
+    // draw beacons
+    svg.append("g")
       .attr("class", "sites")
       .selectAll("circle")
-      .data(sites[level])
-      .enter().append("circle")
-      .attr("r", 5)
-      .call(redrawSite);
-    
-    var sensor = svg.append("g")
-      .attr("class", "sites sensor")
-      .selectAll("circle")
-      .data(getSensorLocationsForLevel(level), sl => sl.sensorId)
+      .data(beacons)
       .enter()
         .append("circle")
-        .attr("r", 10)
-        .call(redrawSensor);
+        .attr("r", 5)
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y)
+        // useful debugging tool: log beacon object to console on click
+        .on('click', d => console.log(d) )
+    
+      // prepare a layer for the sensors, but don't do anything with it just yet
+      svg.append('g').attr('class', 'sensors');
   });
+};
 
-  var getCentroid = function (coords) {
-    var center = coords.reduce(function reduceCoords(x, y) {
-      return [x[0] + y[0] / coords.length, x[1] + y[1] / coords.length]
-    }, [0, 0])
-    return center;
+/**
+ * Groups a list of objects with a 'level' key by level
+ * @param {[Object]} List of objects with a 'level' key 
+ */
+const groupByLevel = objects => {
+  const objectsByLevel = {};
+  objects.forEach(obj => {
+    if (objectsByLevel[obj.level] === undefined) {
+      objectsByLevel[obj.level] = [];
+    }
+    objectsByLevel[obj.level].push(obj);
+  })
+  return objectsByLevel;
+}
+
+const drawSensors = allSensors => {
+  const sensorsByLevel = groupByLevel(allSensors);
+  levels.forEach(level => {
+    const sensors = sensorsByLevel[level] || [];
+    const svg = svgForLevel(level);
+    const sensorGraphics = svg.select('.sensors');
+    const updatedSensorGraphics = sensorGraphics
+      .selectAll('image.sensor')
+      .data(sensors, s => s.id)
+      
+    updatedSensorGraphics.transition()
+      .attr("x", d => d.x)
+      .attr("y", d => d.y)
+
+    updatedSensorGraphics.enter()
+      .append('image')
+      .attr("xlink:href", "ufo.svg?yellow")
+      .attr('class', 'sensor')
+      .attr("x", d => d.x)
+      .attr("y", d => d.y)
+      .attr('width', 0)
+      .attr('height', 0)
+      // .style('fill', '#f00')
+      // .attr('r', 0)
+      .transition()
+      .attr("x", d => d.x - ufoSize / 2)
+      .attr("y", d => d.y - ufoSize / 2)
+      .attr('width', ufoSize)
+      .attr('height', ufoSize)
+    
+    updatedSensorGraphics.exit()
+      .attr("x", d => {console.log(d); return d.x - ufoSize / 2})
+      .attr("y", d => d.y - ufoSize / 2)
+      .transition()
+      .attr('width', 0)
+      .attr('height', 0)
+      .attr("x", d => d.x)
+      .attr("y", d => d.y)
+      .remove()
+  })
+};
+
+d3.json('locations-parsed.json', data => {
+  beaconsByLevel = groupByLevel(data);
+  data.forEach(beacon => {
+    beaconById[beacon.id] = beacon;
+  });
+  drawLevels(beaconsByLevel);
+  // drawSensors(sensors);
+  // setInterval(
+  //   () => {
+  //     sensors[0].x = Math.random() * 600;
+  //     sensors[0].y = Math.random() * 150;
+  //     const proposedLevel = sensors[0].level + Math.round(Math.random() * 2 - 1);
+  //     sensors[0].level = Math.max(-2, Math.min(3, proposedLevel));
+  //     drawSensors(sensors);
+  //   },
+  //   1000
+  // );
+});
+
+const getCentroid = coords => coords.reduce((acc, c) => ({
+  x: acc.x + c.x / coords.length,
+  y: acc.y + c.y / coords.length,
+}), { x: 0, y: 0 });
+
+
+const getLocation = beacons => {
+  // majority vote on level: if we get 2 beacons on the 1st floor and 1 on the
+  // 2nd floor, assume the sensor is on the 1st floor
+  let bestCount = 0;
+  let bestLevel = 0;
+  let counts = {};
+  const filteredBeacons = beacons
+    .sort((a, b) => b.rssi - a.rssi)
+    .slice(0, 3)
+  filteredBeacons.forEach(beacon => {
+    let level = beaconById[beacon.krookid].level;
+    counts[level] = (counts[level] || 0) + 1;
+    if (counts[level] > bestCount) {
+      bestCount = counts[level];
+      bestLevel = level;
+    }
+  });
+  const coords = getCentroid(filteredBeacons.map(b => beaconById[b.krookid]));
+  return {
+    x: coords.x,
+    y: coords.y,
+    level: bestLevel,
+    beacons: beacons,
   };
 
-  function getCoord(beacons) {
-    // This code gets the centroid of the three closest beacons to the device, e.g. a sort of fake location
-    var closest = beacons.sort((a, b) => b.rssi - a.rssi).slice(0, 3)
-    var positions = locations;
+}
 
-    var closestNodeCoords = closest.map(c => {
-      var position = positions[c.krookid];
-      return [position.x, position.y];
-    });
-    // movedFuncs[level](getCentroid(closestNodeCoords));
-    return getCentroid(closestNodeCoords);
-  }
-
-  var length = function length(l) {
-    var s = l.source;
-    var t = l.target;
-    return Math.sqrt((s[0] - t[0]) * (s[0] - t[0]) + (s[1] - t[1]) * (s[1] - t[1]));
-  };
-
-  var getClosestSites = function getClosestSites(level, coord, n) {
-    var allSites = sites[level].map(x => x);
-    allSites.sort((a, b) => length({ source: coord, target: a }) - length({ source: coord, target: b }));
-    return allSites.slice(0, n);
-  };
-  /**
-   * Maps a list of beacons to a { coords: [x, y], level: x } object
-   * @param {beacons} beacons list of beacons in { rssi:x, krookid: x} format
-   */
-  var getSensorLocation = function getSensorLocation(sensorId, beacons) {
-    // majority vote on level: if we get 2 beacons on the 1st floor and 1 on the
-    // 2nd floor, assume the sensor is on the 1st floor
-    var bestCount = 0;
-    var bestLevel = 0;
-    var counts = {};
-    beacons.forEach(function eachBeacon(beacon) {
-      var level = siteToLevel[beacon.krookid];
-      counts[level] = (counts[level] || 0) + 1;
-      if (counts[level] > bestCount) {
-        bestCount = counts[level];
-        bestLevel = level;
+db.ref('/').on('value', snapshot => {
+  const rawData = snapshot.val();
+  console.log(rawData);
+  const sensors = []
+  Object.keys(rawData).forEach(sensorId => {
+    const sensor = rawData[sensorId];
+    const beacons = Object.keys(sensor).map(hexId => {
+      const beaconData = sensor[hexId];
+      return {
+        rssi: beaconData.RSSI,
+        krookid: parseInt(hexId, 16),
       }
     });
-    var coords = getCoord(beacons);
-    return {
-      sensorId: sensorId,
-      coords: coords,
-      level: bestLevel,
-      sites: beacons.map(b => b.krookid),
-      closestSites: getClosestSites(bestLevel, coords, 3),
-    };
-
-  }
-
-  var updateSensors = function updateSensors(sensorId, beacons) {
-    var location = getSensorLocation(sensorId, beacons);
-    console.log(sensorId, location);
-    sensors[sensorId] = location;
-    Object.keys(movedFuncs).forEach(function callMovedFunc(level) {
-      movedFuncs[level]();
-    });
-    
-  }
-
-  db.ref('/').on('value', function (snapshot) {
-    const rawData = snapshot.val();
-    console.log(rawData);
-    Object.keys(rawData).forEach(sensorId => {
-      const sensor = rawData[sensorId];
-      const processedData = Object.keys(sensor).map(hexId => {
-        const beaconData = sensor[hexId];
-        return {
-          rssi: beaconData.RSSI,
-          krookid: parseInt(hexId, 16),
-        }
-      });
-      updateSensors(sensorId, processedData);
-    });
-  });
-}
-var locations;
-d3.json('locations-parsed.json', function (data) {
-  locations = data;
-  for (var i = 0; i < data.length; i++) {
-    if (sites[data[i].level] == undefined) {
-      sites[data[i].level] = []
+    const sensorLocation = getLocation(beacons);
+    const processedData = {
+      id: sensorId,
+      name: sensorId,
+      level: sensorLocation.level,
+      x: sensorLocation.x,
+      y: sensorLocation.y,
+      beacons: beacons,
     }
-    sites[data[i].level].push([data[i].x, data[i].y]);
-    siteToLevel[data[i].id] = data[i].level;
-  }
-  init();
+    sensors.push(processedData);
+    console.log('loc', sensorLocation);
+  });
+  drawSensors(sensors);
 });
