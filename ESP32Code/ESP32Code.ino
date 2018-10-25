86ae295e03b29476684f15a680dcf41a99c9722d
jjuchem <Jasper.Juchem@UGent.be>
2018-10-25 22:46:35 +0200

    Update Arduino
    Arduino code is updated. Change WiFi settings for use in Krook.

--- ESP32Code/ESP32Code.ino
+++ ESP32Code/ESP32Code.ino
@@ -1,5 +1,6 @@
 
 #include <Esp.h>
+#include <driver/adc.h>
 #include <Arduino.h>
 #include <WiFi.h>
 #include <WiFiMulti.h>
@@ -9,26 +10,28 @@
 #include <BLEScan.h>
 #include <BLEAdvertisedDevice.h>
 
-#define ANALOG_BatteryPower 26 //A0
-#define LED_BUILTIN 2 //13
+#define ANALOG_BatteryPower 34 //A2
+#define LED_BUILTIN 13
 
 /* Esp settings */
 int scanTime = 2; //in seconds 
 int sleepTime = 5; // in seconds
+
 /* backend settings */ 
 String firebaseLink = "http://krookfirebase.barkr.uk/";
-String deviceName = "ufo-3";
-String batteryName = "batt-3";
+String deviceName;
+String batteryName;
+
 /* Wifi settings */
 // Krook WiFi
-char ssid[] = "***REMOVED***";
-char password[] = "***REMOVED***";
-// Nerdlab WiFi
-/*char ssid[] = "Nerdlab";
+/*char ssid[] = "***REMOVED***";
 char password[] = "***REMOVED***";*/
+// Nerdlab WiFi
+char ssid[] = "Nerdlab";
+char password[] = "***REMOVED***";
 /* end settings */
 
-
+/*Init*/
 BLEScan* pBLEScan = BLEDevice::getScan(); 
 WiFiMulti wifiMulti;
 HTTPClient http;
@@ -36,7 +39,6 @@ String json;
 int batteryLevel_Digital=0;
 float batteryLevel_Analog=0.0;
 String batLevel;
-float randomValue=0.0;
 
 class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
     void onResult(BLEAdvertisedDevice advertisedDevice) {
@@ -55,13 +57,19 @@ void setup() {
     // Set serial console
     Serial.begin(115200);
     pinMode(LED_BUILTIN, OUTPUT);
-    
+
+    String macad=getMacAddress();
+    Serial.print("Mac: ");
+    Serial.println(macad);
+    deviceName=macad;
+    batteryName=macad;
+        
     // Set analogRead
     analogReadResolution(11);
     analogSetAttenuation(ADC_6db);
     
     // Set WiFi
-    wifiMulti.addAP(ssid, password); 
+    wifiMulti.addAP(ssid, password);
     BLEDevice::init("");
     pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
     pBLEScan->setActiveScan(true);
@@ -103,27 +111,28 @@ void batteryLevel() {
   |                            Acquire Battery Level                                    |
   **************************************************************************************/
   // Read battery voltage in ADC form - 11bits (0-2047)
-  batteryLevel_Digital=analogRead(ANALOG_BatteryPower);
-  
-  //randomValue=random(100)/100.0; // Remove when done testing - add random value to see change
+  batteryLevel_Digital=readLightSensor();
+  Serial.print("Batt: ");
+  Serial.print(batteryLevel_Digital);
+  Serial.print(" - ");
 
   // Convert battery level from digital to analog signal and make string
-  batteryLevel_Analog=map(batteryLevel_Digital,0,2047,0,35)/10.0;//+randomValue;
-  batLevel="{\"Level_bat3\":"+String(batteryLevel_Analog)+"}";
+  batteryLevel_Analog=map(batteryLevel_Digital,0,2047,0,36)/10.0;//+randomValue;
+  batLevel=String(batteryLevel_Analog);
+  Serial.println(batLevel);
 }
 
 void pushToFirebase(){
       if((wifiMulti.run() == WL_CONNECTED)) {           // Check if WiFi is connected
         // Connect to URL for RSSI values - http://krookfirebase.barkr.uk/ufo-1.json
-        
+                
         http.begin(firebaseLink + "sensors/" + deviceName + ".json"); //HTTP
         http.addHeader("Content-Type", "application/x-www-form-urlencoded");
         int httpCode_json = http.PUT(json);
+
+        Serial.println(firebaseLink + "sensors/" + deviceName + ".json");
+        Serial.println(json);
         
-        // Debugging prints
-        /*Serial.println("PUT2: "+String(httpCode_json));
-        Serial.println("The .json: "+json);
-        Serial.println(firebaseLink + deviceName + ".json");*/
         // Check succes of PUT
         if(httpCode_json > 0) {
           if(httpCode_json == HTTP_CODE_OK) {
@@ -137,14 +146,10 @@ void pushToFirebase(){
         
         // Connect to URL for battery level - http://krookfirebase.barkr.uk/batt-1.json
         
-        http.begin(firebaseLink + "battery/" + batteryName + ".json"); //HTTP
+        http.begin(firebaseLink + "battery/"+deviceName+".json"); //HTTP
         http.addHeader("Content-Type", "application/x-www-form-urlencoded");
         int httpCode_battery = http.PUT(batLevel);
 
-        //Debugging prints
-        /*Serial.println("PUT3: "+String(httpCode_battery));
-        Serial.println("Battery Level: "+batLevel);
-        Serial.println(firebaseLink + batteryName + ".json");*/
         // Check succes of PUT
         if(httpCode_battery > 0) {
           if(httpCode_battery == HTTP_CODE_OK) {
@@ -156,6 +161,21 @@ void pushToFirebase(){
         }
         http.end();       // end connection with http://krookfirebase.barkr.uk/batt-1.json
       }
+}
+
+String getMacAddress(){
+  uint8_t baseMac[6];
+  esp_read_mac(baseMac, ESP_MAC_WIFI_STA);
+  baseMac[5]=baseMac[5] & 0b11111100;
+  char baseMacChr[18] = {0};
+  sprintf(baseMacChr, "%02X:%02X:%02X:%02X:%02X:%02X", baseMac[0],baseMac[1],baseMac[2],baseMac[3],baseMac[4],baseMac[5]);
+  return String(baseMacChr);
+}
+
+int readLightSensor(){
+  //adc1_config_width(ADC_WIDTH_BIT_10);   //Range 0-1023 
+  //adc1_config_channel_atten(ANALOG_BatteryPower,ADC_ATTEN_DB_11);  //ADC_ATTEN_DB_11 = 0-3,6V
+  return analogRead( ANALOG_BatteryPower ); //Read analog
 }
       
 
