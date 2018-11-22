f19a3c65093156a176bd31a2568c26f7ca0c64d2
Michiel Van den Berghe <michiel@trackuity.com>
2018-11-22 20:55:57 +0100

    Add simple goal logic

---
+++ functions/pois.js
@@ -0,0 +1,20 @@
+module.exports = {
+    'data-wall': {
+        // name: 'Data wall',
+        objectiveText: 'Breng een alien naar de data wall!',
+        location: { level: 0, x: 974.0799999999999, y: 73.34905660377358 },
+        radius: 50,
+    },
+    'ingang': {
+        // name: 'Ingang',
+        objectiveText: 'Breng een alien naar de ingang!',
+        location: { level: 0, x: 1514.8799999999999, y: 173.5849056603774 },
+        radius: 50,
+    },
+    'kleine-prins': {
+        // name: 'De Kleine Prins'
+        objectiveText: 'Breng een alien naar "De Kleine Prins" van Antoine de st. Exupéry!',
+        location: { level: -1, x: 176.64, y: 128.77358490566039 },
+        radius: 50,
+    },
+};
